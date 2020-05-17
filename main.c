90c7550aaa42b004af0f561a544ebaec796b5f1b
Vadim Ostafiychuk <vadym.ostafiychuk@riseserv.com>
2020-05-17 17:29:51 +0300

    Release 1.0-l1

---
+++ main.c
@@ -0,0 +1,37 @@
+﻿#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <time.h>
+
+int main() {
+	setlocale(LC_ALL, "ua");
+
+	exOne();
+	
+	
+	printf("\n\n");
+	system("pause");
+	return 0;
+}
+
+exOne() {
+	int a,b,c,cOne = 0,aOne = 0,bOne = 0;
+	printf("Введiть число A = ");
+	scanf("%d",&a);
+	printf("Введiть число B = ");
+	scanf("%d",&b);
+	printf("Введiть число C = ");
+	scanf("%d",&c);
+	
+	bOne = b;
+	cOne = c;
+	aOne = a;
+	b = a;
+	c = bOne;
+	a = cOne;
+
+	printf("A = %d B = %d C = %d", a,b,c);
+
+}
